f367c6ab5d60a76f370ead028a488581e31176c9
Lucamusumeci <lucamusumeci@live.com>
2023-01-14 17:44:18 +0100

    recipe/show template, properties added.

--- spring5-recipe-assignment-web/src/test/java/org/lucamusumeci/spring5recipeassignment/controllers/RecipeControllerTest.java
+++ spring5-recipe-assignment-web/src/test/java/org/lucamusumeci/spring5recipeassignment/controllers/RecipeControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
@@ -20,6 +21,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 class RecipeControllerTest {
     RecipeController recipeController;
@@ -41,7 +43,8 @@ class RecipeControllerTest {
         //then
         mockMvc.perform(get("/recipe/all"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/list"));
+                .andExpect(view().name("recipe/list"))
+                .andExpect(model().attributeExists("recipe"));
     }
 
     @Test
